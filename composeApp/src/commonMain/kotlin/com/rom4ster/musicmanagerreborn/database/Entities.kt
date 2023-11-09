4e5c9b69a653af18b00794621c9cdc97d3f7c078
rom4ster <rohansampat97@gmail.com>
2023-11-09 01:22:37 -0600

    added DB Entities

--- composeApp/src/commonMain/kotlin/com/rom4ster/musicmanagerreborn/database/Entities.kt
+++ composeApp/src/commonMain/kotlin/com/rom4ster/musicmanagerreborn/database/Entities.kt
@@ -1,6 +1,38 @@
 package com.rom4ster.musicmanagerreborn.database
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 
+@Serializable
+data class AlbumInfo(
+    val artist: String,
+    @SerialName("release-date")
+    val releaseDate: String,
+    @SerialName("album-name")
+    val albumName: String? = null
+)
 
+@Serializable
+data class SongMetadata(
+    val genres: List<String>
+)
 
+@Serializable
+data class Song(
+    val id: String,
+    val name: String,
+    val filePath: String?,
+    val uploader: String?,
+    val ignoreSync: Boolean = false,
+    val info: AlbumInfo? = null,
+    val metadata: SongMetadata? = null
+)
+
+@Serializable
+data class UserPlaylist(
+    val id: String,
+    @SerialName("removed-songs")
+    val removedSongs: List<String> = listOf(),
+    val songs: List<String> = listOf(),
+)
--- composeApp/src/commonMain/kotlin/com/rom4ster/musicmanagerreborn/utils/UUIDUtils.kt
+++ composeApp/src/commonMain/kotlin/com/rom4ster/musicmanagerreborn/utils/UUIDUtils.kt
@@ -1,20 +1,30 @@
 package com.rom4ster.musicmanagerreborn.utils
 
 import com.benasher44.uuid.UuidHasher
+import com.benasher44.uuid.nameBasedUuidOf
+import com.benasher44.uuid.uuidFrom
+import org.kotlincrypto.hash.sha1.SHA1
 
 private const val UUID_VERSION = 5
 
 private const val UUID5_X500_NAMESPACE = "6ba7b814-9dad-11d1-80b4-00c04fd430c8"
 
+private fun <T> T.generateUUID() = this generateUUID {it.toString()}
+
+private infix fun <T> T.generateUUID(block: (T)->String) = nameBasedUuidOf(
+    uuidFrom(UUID5_X500_NAMESPACE),
+    block(this),
+    MMUUIDHasher(UUID_VERSION)
+)
+
 
 data class MMUUIDHasher(override val version: Int) : UuidHasher {
 
     private val digest = SHA1()
-    private val byteStore = byteArrayOf()
     override fun digest(): ByteArray = digest.digest()
 
-    override fun update(input: ByteArray) {
+    override fun update(input: ByteArray) =
         digest.update(input)
-    }
+
 
 }
